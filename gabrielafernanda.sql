f4ad8256ec7f4e2909351c586a32dfceda9883e9
Gabriela Fernanda <gabriela.f.barbosa@aluno.senai.br>
2024-11-05 16:42:03 -0300

    first commit

---
+++ gabrielafernanda.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE clinica;
+
+CREATE TABLE pacientes (
+    id_paciente SERIAL PRIMARY KEY.
+    nome_paciente VARCHAR (50),
+    email_paciente VARCHAR (50),
+    telefone_paciente VARCHAR (50)
+);
+
